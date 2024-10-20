48dc9f066005e2ea35ca741f80df6856394fe08e
fredved1 <11spotifygratis@gmail.com>
2024-10-20 09:22:06 +0200

    onderhoudspagina toegevoegd

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,15 @@
 import UWVChatbot from '../components/UWVChatbot'
+import MaintenancePage from '../components/MaintenancePage' // Zorg ervoor dat je deze component maakt
+
+const isUnderMaintenance = true;
 
 export default function Home() {
+  if (isUnderMaintenance) {
+    return <MaintenancePage />;
+  }
+
+  // Originele code (behouden door commentaar)
+  // return <UWVChatbot />
+
   return <UWVChatbot />
 }
---
+++ components/MaintenancePage.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const MaintenancePage: React.FC = () => {
+  return (
+    <div>
+      <h1>Onderhoud</h1>
+      <p>Onze site is momenteel niet beschikbaar vanwege onderhoud. Probeer het later opnieuw.</p>
+    </div>
+  )
+}
+
+export default MaintenancePage
