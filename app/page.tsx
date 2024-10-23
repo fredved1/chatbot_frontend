66e7f9cf81cfcf0f1209e9a71ba2d79963dcaff9
fredved1 <131248542+fredved1@users.noreply.github.com>
2024-10-23 15:44:53 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import UWVChatbot from '../components/UWVChatbot'
 import MaintenancePage from '../components/MaintenancePage' // Zorg ervoor dat je deze component maakt
 
-const isUnderMaintenance = false;
+const isUnderMaintenance = true;
 
 export default function Home() {
   if (isUnderMaintenance) {
